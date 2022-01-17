e8f365e2124f35cee044466e424726b47212ab20
Duc Anh <bdadevfs@gmail.com>
2022-01-17 20:51:23 +0700

    Insert-in-database-table

--- app/src/main/java/com/example/booklibrary/AddActivity.java
+++ app/src/main/java/com/example/booklibrary/AddActivity.java
@@ -3,12 +3,31 @@ package com.example.booklibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddActivity extends AppCompatActivity {
-
+    EditText inputTitle, inputAuthor, inputPages;
+    Button addBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        inputTitle = findViewById(R.id.tittle_input);
+        inputAuthor = findViewById(R.id.author_input);
+        inputPages = findViewById(R.id.pages_input);
+
+        addBtn = findViewById(R.id.addtolibraryBtn);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyDatabaseHelper myDB = new MyDatabaseHelper(AddActivity.this);
+                myDB.addBook(inputTitle.getText().toString().trim(),
+                        inputAuthor.getText().toString().trim(),
+                        Integer.valueOf(inputPages.getText().toString().trim()));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/booklibrary/MyDatabaseHelper.java
+++ app/src/main/java/com/example/booklibrary/MyDatabaseHelper.java
@@ -1,8 +1,10 @@
 package com.example.booklibrary;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -24,19 +26,35 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     }
 
     @Override
+
     public void onCreate(SQLiteDatabase db) {
         String query =
                 "CREATE TABLE " + TABLE_NAME +
-                        " (" + COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                        COLUMN_TITLE + " TEXT, " +
-                        COLUMN_AUTHOR + " TEXT, " +
-                        COLUMN_PAGES + "INTEGER);";
+                        " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_TITLE + " TEXT, " +
+                COLUMN_AUTHOR + " TEXT, " +
+                COLUMN_PAGES + " INTEGER);";
         db.execSQL(query);
     }
-
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+
+    void addBook(String title, String author, int pages){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_TITLE,title);
+        cv.put(COLUMN_AUTHOR,author);
+        cv.put(COLUMN_PAGES,pages);
+        long result = db.insert(TABLE_NAME,null,cv);
+        if(result == -1){
+            Toast.makeText(context,"Failed",Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(context,"Added successfully!",Toast.LENGTH_SHORT).show();
+        }
     }
 }
