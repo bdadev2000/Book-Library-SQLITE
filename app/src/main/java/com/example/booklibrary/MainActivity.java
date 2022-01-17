952979ec3e9d550b4000c5ce11e884e98ea1121f
Duc Anh <bdadevfs@gmail.com>
2022-01-17 23:13:59 +0700

    Display-data-in-recycleview

--- app/src/main/java/com/example/booklibrary/MainActivity.java
+++ app/src/main/java/com/example/booklibrary/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.booklibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     FloatingActionButton addBtn;
+
+    MyDatabaseHelper myDB;
+    ArrayList<String> bookID, bookTitle, bookAuthor, bookPages;
+    CustomAdapter customAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +35,32 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        myDB = new MyDatabaseHelper(MainActivity.this);
+        bookID = new ArrayList<>();
+        bookTitle = new ArrayList<>();
+        bookAuthor = new ArrayList<>();
+        bookPages = new ArrayList<>();
+
+        storeDataInArray();
+        customAdapter = new CustomAdapter(MainActivity.this, bookID, bookTitle, bookAuthor, bookPages);
+        recyclerView.setAdapter(customAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
     }
+
+    void storeDataInArray(){
+        Cursor cursor = myDB.readAllData();
+        if(cursor.getCount() == 0){
+            Toast.makeText(this, "No data", Toast.LENGTH_SHORT).show();
+        }else {
+            while(cursor.moveToNext()){
+                bookID.add(cursor.getString(0));
+                bookTitle.add(cursor.getString(1));
+                bookAuthor.add(cursor.getString(2));
+                bookPages.add(cursor.getString(3));
+            }
+        }
+
+    }
+
 }
